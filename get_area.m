05630579995220bd7a39ab0332748a13a81b0224
User <User@DESKTOP-FUVGBKM>
2017-05-01 12:28:46 +0300

    fiished modifying code to take in rect

--- get_area.m
+++ get_area.m
@@ -1,4 +1,4 @@
-function out=get_area(data)
+function sub_ind=get_area(data)
 %data0=repmat([1:85.^2]',1,3000 );
 %data0=reshape([1:3000.*(85.^2)]',85.^2,3000);
 %data0=data0./max(data0(:));
@@ -11,7 +11,7 @@ rect=round(getrect);
 rectangle('Position',rect);
 ind_mat=reshape([1:85.^2],85,85);
 sub_ind=ind_mat(rect(1)+[0:rect(3)-1], rect(2)+[0:rect(4)-1]);
-sub_set=data(sub_ind(:),:);
+% sub_set=data(sub_ind(:),:);
 %figure
 %imshow(reshape(mean(sub_set'),rect(4),rect(3)),[])
 
--- raw_signal.m
+++ raw_signal.m
@@ -4,7 +4,7 @@ clc
 % convert frames in cell/structure format to 2D matrix
      %Dir=['/Users/nadavivzan/Dropbox/Mandel lab/VSDI_code/monitor_0/'];  
      Dir=['C:\Users\User\Dropbox\Mandel lab\VSDI data\2017.04.19\E3'];
-     mtx=frms2mat(Dir); 
+%     mtx=frms2mat(Dir); 
      %load E3.mat 
 %   
 [t_trig0, t_photod0]=find_trg([Dir '\E3.bin']);
@@ -24,11 +24,18 @@ end
 
 ints_pre=[];
 figure
+
+
 n=5208; %% pixel vector I choose to show
-nf=400; %number of frames to show
+nf=200; %number of frames to show
+
+%sub_ind=get_area(mtx{1}); get area under rectangle
+
 for k0=1:10
-    indx_pre=round(t0(k0).*250)+[1:nf];     
-    ints_pre=[ints_pre; mtx{k0}(n,indx_pre)];
+    sub_set{k0}=mtx{k0}(sub_ind(:),:);
+    indx_pre=round(t0(k0).*250)+[1:nf];   
+    
+    ints_pre=[ints_pre; mean(sub_set{k0}(:,indx_pre))];
 end
 
 base_line=mean(ints_pre,2);
@@ -37,7 +44,7 @@ base_line=mean(ints_pre,2);
 
 for k0=1:length(mtx)
     
-   v0= (mtx{k0}(n,:)-base_line(k0))./base_line(k0);
+   v0= (mean(sub_set{k0})-base_line(k0))./base_line(k0);
    v1=2.*(v0-min(v0))./(max(v0)-min(v0))-2.*k0+1;
    %figure 
    %subplot(10,1,k0)
